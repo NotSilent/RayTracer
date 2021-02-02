60a930e9fd0f7ae2f46d980f135d761d54d88856
silent <keiya94@gmail.com>
2021-02-02 18:06:32 +0100

    Chapter 9: Challenge

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <Camera.h>
 #include <Canvas.h>
 #include <fstream>
+#include <Plane.h>
 
 int main() {
     auto t1 = std::chrono::high_resolution_clock::now();
@@ -16,32 +17,23 @@ int main() {
     const Material green(Color(0.0f, 1.0f, 0.0f),
                          0.1f, 0.5f, 5.0f, 200.0f);
 
-    const Sphere floor(Mat4::scaling(10.0f, 0.01f, 10.0f), red);
-    const Sphere leftWall(Mat4::translation(0.0f, 0.0f, 5.0f) *
-                          Mat4::rotation<Axis::Y>(-FMath::PI / 4.0f) *
-                          Mat4::rotation<Axis::X>(FMath::PI / 2.0f) *
-                          Mat4::scaling(10.0f, 0.01f, 10.0f),
-                          blue);
-    const Sphere rightWall(Mat4::translation(0.0f, 0.0f, 5.0f) *
-                           Mat4::rotation<Axis::Y>(FMath::PI / 4.0f) *
-                           Mat4::rotation<Axis::X>(FMath::PI / 2.0f) *
-                           Mat4::scaling(10.0f, 0.01f, 10.0f),
-                           blue);
-    const Sphere middle(Mat4::translation(-0.5f, 1.0f, 0.5f), green);
-    const Sphere right(Mat4::translation(1.5f, 0.5f, -0.5f) *
-                       Mat4::scaling(0.5f, 0.5f, 0.5f), green);
-    const Sphere left(Mat4::translation(-1.5f, 0.33f, -0.75f) *
-                      Mat4::scaling(0.33f, 0.33f, 0.33f), green);
+    const auto floor = std::make_shared<Plane>(red);
+    const auto middle = std::make_shared<Sphere>(
+            Mat4::translation(-0.5f, 1.0f, 0.5f), green);
+    const auto right = std::make_shared<Sphere>(
+            Mat4::translation(1.5f, 0.5f, -0.5f) *
+            Mat4::scaling(0.5f, 0.5f, 0.5f), green);
+    const auto left = std::make_shared<Sphere>(
+            Mat4::translation(-1.5f, 0.33f, -0.75f) *
+            Mat4::scaling(0.33f, 0.33f, 0.33f), green);
     World w;
-    w.addLight(PointLight(Tuple::point(-10.0f, 10.0f, -10.0f),
+    w.setLight(PointLight(Tuple::point(-10.0f, 10.0f, -10.0f),
                           Color(1.0f, 1.0f, 1.0f)));
     w.addObject(floor);
-    w.addObject(leftWall);
-    w.addObject(rightWall);
     w.addObject(middle);
     w.addObject(right);
     w.addObject(left);
-    Camera c(1920, 1080, FMath::PI / 3.0f,
+    Camera c(640, 480, FMath::PI / 3.0f,
              Mat4::getViewTransform(
                      Tuple::point(0.0f, 1.5f, -5.0f),
                      Tuple::point(0.1f, 1.0f, 0.0f),
